bcd91b9a4e39146145aa26bae5e6d7f0c181511e
wangjugang521125 <103366744+wangjugang521125@users.noreply.github.com>
2022-04-11 01:40:51 +0800

    Update AppMember.java
    风格的人

--- mayikt-service-impl/mayikt-member-service-impl/src/main/java/com/mayikt/AppMember.java
+++ mayikt-service-impl/mayikt-member-service-impl/src/main/java/com/mayikt/AppMember.java
@@ -8,5 +8,7 @@ public class AppMember {
 
     public static void main(String[] args) {
         SpringApplication.run(AppMember.class);
+        System.out.println("hhhhhhhh啊啊啊啊");
+        
     }
 }

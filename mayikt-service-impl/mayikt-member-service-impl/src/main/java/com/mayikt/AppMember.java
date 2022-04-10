daeb91c34baa0fa458193b3b05b6713c66843e5e
wangjugang521125 <103366744+wangjugang521125@users.noreply.github.com>
2022-04-11 01:36:47 +0800

    Update AppMember.java
    远程库修改

--- mayikt-service-impl/mayikt-member-service-impl/src/main/java/com/mayikt/AppMember.java
+++ mayikt-service-impl/mayikt-member-service-impl/src/main/java/com/mayikt/AppMember.java
@@ -8,5 +8,8 @@ public class AppMember {
 
     public static void main(String[] args) {
         SpringApplication.run(AppMember.class);
+        System.out.println("hhhhhhhhddddd");
+        
+        
     }
 }
